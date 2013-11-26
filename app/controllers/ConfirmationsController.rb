594f89c604e04f89122acd6082f16266c7ca0bc6
hassanakram <root@bt.foo.org>
2013-11-26 22:55:25 +0000

    error remove

--- app/controllers/ConfirmationsController.rb
+++ app/controllers/ConfirmationsController.rb
@@ -2,6 +2,5 @@ class ConfirmationsController < Devise::ConfirmationsController
   protected
     def after_confirmation_path_for(resource_name, resource)
         '/home/default'
-      end
     end
 end
